db6b51f0f0bd0530170e0267674e2f6e42db3f29
Nick <n.chornyi666@gmail.com>
2021-09-02 21:52:05 +0300

    correct name variable

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,27 +5,27 @@ import org.example.frames.SortFrame;
 
 public class App {
 
-    private IntroFrame mainFrame;
-    private SortFrame numbersFrame;
+    private IntroFrame introFrame;
+    private SortFrame sortFrame;
 
     public static void main(String[] args) {
         new App().run();
     }
 
     private void run() {
-        mainFrame = new IntroFrame(this);
-        mainFrame.setVisible(true);
+        introFrame = new IntroFrame(this);
+        introFrame.setVisible(true);
     }
 
     public void showMain() {
-        numbersFrame.setVisible(false);
-        mainFrame.setVisible(true);
+        sortFrame.setVisible(false);
+        introFrame.setVisible(true);
     }
 
     public void showNumbers(int count) {
-        numbersFrame = new SortFrame(this);
-        mainFrame.setVisible(false);
-        numbersFrame.setVisible(true);
-        numbersFrame.generatePanelNumbers(count);
+        sortFrame = new SortFrame(this);
+        introFrame.setVisible(false);
+        sortFrame.setVisible(true);
+        sortFrame.generatePanelNumbers(count);
     }
 }
